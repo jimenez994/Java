be418ee8d00f1ca8e578de91bf69573d2d8e7a7f
Jonathan Jimenez <j.jimenez.994@hotmail.com>
2018-04-02 19:39:33 -0400

    _

---
+++ javaR/operatorsInJava/sum.java
@@ -0,0 +1,6 @@
+public class sum{
+    public static void sum(int num1, int num2){
+        System.out.println(num1 + num2);
+    }
+
+}
---
+++ javaR/operatorsInJava/test.java
@@ -0,0 +1,14 @@
+/**
+ * test
+ */
+class test {
+    public static void main(String args[]){
+        System.out.println("yo");
+        int x = 2;
+        int y = 5;
+        sum s = new sum();
+        s.sum(x,y);
+        s.sum(50,50);
+        
+    }
+}
