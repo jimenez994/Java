8269503500af401ec19b77cf3225251c2992650f
JJimenez <j.jimenez.994@hotmail.com>
2019-10-20 21:33:31 -0400

    _

--- JavaCurrent/HelloWorld/src/Hello.java
+++ JavaCurrent/HelloWorld/src/Hello.java
@@ -2,12 +2,13 @@ public class Hello {
     public static void main(String[] args){
         String firstName = "Jonathan";
         String lastName = "Jimenez";
-        int age = 24;
+        int age = (2+ 3) + (10 + 9);
+        int firstN = 12;
+        int secondN = 6;
+        System.out.println(firstN + secondN + age);
         System.out.println("Hello, " + firstName +" " + lastName + " " + age);
-        System.out.println(age);
-
-
         age = add(age);
+        System.out.println(age);
     }
     public static int add(int in){
         System.out.println(in + 1);
