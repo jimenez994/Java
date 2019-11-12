e6c6a62ec4c454b41851e219bd87c71ec4ab01b6
JJimenez <j.jimenez.994@hotmail.com>
2019-11-11 22:26:17 -0500

    _

---
+++ JavaCurrent/FlourPack/src/zeus/jim/Main.java
@@ -0,0 +1,12 @@
+package zeus.jim;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+    }
+    public static void canPack(int smallCount, int bigCount, int goal){
+
+    }
+}
