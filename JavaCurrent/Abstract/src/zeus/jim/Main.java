ea9eefaf5630adc415b9b0bc9090e82451bb9795
JJimenez <j.jimenez.994@hotmail.com>
2020-01-04 21:18:45 -0500

    -

--- JavaCurrent/Abstract/src/zeus/jim/Main.java
+++ JavaCurrent/Abstract/src/zeus/jim/Main.java
@@ -3,6 +3,14 @@ package zeus.jim;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        MyLinkedList list = new MyLinkedList(null);
+        list.traverse(list.getRoot());
+        String myString = "Bolivia Ecuador Chile Brazil Colombia Bolivia Peru Argentina Mexico Spana Russia Honduras";
+        String[] data  = myString.split(" ");
+        for (String word:data) {
+            list.addItem(new Node(word));
+        }
+        list.traverse(list.getRoot());
+//        list.
     }
 }
---
+++ JavaCurrent/Abstract/src/zeus/jim/MyLinkedList.java
@@ -0,0 +1,91 @@
+package zeus.jim;
+
+public class MyLinkedList implements NodeList {
+    private ListItem root = null;
+
+    public MyLinkedList(ListItem root) {
+        this.root = root;
+    }
+
+    @Override
+    public ListItem getRoot() {
+        return this.root;
+    }
+
+    @Override
+    public boolean addItem(ListItem newItem) {
+//        System.out.println(newItem.getValue() + " -");
+        if (this.root == null){
+            this.root = newItem;
+            return true;
+        }
+        ListItem currentItem = this.root;
+        while (currentItem != null){
+            int comparison = currentItem.compareTo(newItem);
+            if (comparison < 0){
+//                newItem is no next, so insert at the end of list
+                if(currentItem.next() != null){
+                    currentItem = currentItem.next();
+                }else{
+                    currentItem.setNext(newItem);
+                    newItem.setPrevious(currentItem);
+                    return true;
+                }
+            }else if( comparison > 0){
+//                newItem is less, insert before
+                if(currentItem.previous() != null){
+                    currentItem.previous().setNext(newItem);
+                    newItem.setPrevious(currentItem.previous());
+                    newItem.setNext(currentItem);
+                    currentItem.setPrevious(newItem);
+                }else{
+//                    the node with a previous is the root
+                    newItem.setNext(this.root);
+                    this.root.setPrevious(newItem);
+                    this.root = newItem;
+                }
+                return true;
+            }else{
+                System.out.println(newItem.getValue() + "is already present, not added");
+                return false;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean removeItem(ListItem item) {
+        if(item != null){
+            System.out.println("Deleting item " + item.getValue());
+        }
+        ListItem currentItem = this.root;
+        while (currentItem != null){
+            int comparison = currentItem.compareTo(item);
+            if(comparison == 0 ){
+                if(currentItem == this.root){
+                    this.root = currentItem.next();
+                }else{
+
+                }
+            }
+        }
+        item.previous().setNext(item.next());
+        item.next().setPrevious(item.previous());
+        item.setPrevious(null);
+        item.setNext(null);
+        item = null;
+        return false;
+    }
+
+    @Override
+    public void traverse(ListItem root) {
+        if(root == null){
+            System.out.println("The list is empty");
+        }else{
+            while(root != null){
+                System.out.println(root.getValue());
+                root = root.next();
+            }
+        }
+    }
+}
