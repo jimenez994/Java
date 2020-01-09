59ee5e1c5ed58140f3f755143a63e133f03d997a
JJimenez <j.jimenez.994@hotmail.com>
2020-01-08 21:48:07 -0500

    packages

--- JavaCurrent/AlgosPackageTest/src/com/zeus/jim/Main.java
+++ JavaCurrent/AlgosPackageTest/src/com/zeus/jim/Main.java
@@ -7,5 +7,15 @@ public class Main {
         System.out.println(Series.fibonacci(5));
         System.out.println(Series.factorial(5));
         System.out.println(Series.nSum(5));
+
+        for (int i = 0; i <=10; i++){
+            System.out.println("Sum "+Series.nSum(i));
+        }
+        for (int i = 0; i <=10; i++){
+            System.out.println("Factorial "+Series.factorial(i));
+        }
+        for (int i = 0; i <=10; i++){
+            System.out.println("Fibonacci "+Series.fibonacci(i));
+        }
     }
 }
