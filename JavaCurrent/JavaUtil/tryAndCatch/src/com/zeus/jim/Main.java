4b37bebda00697c25d5cf1617afd54af70c505ee
JJimenez <j.jimenez.994@hotmail.com>
2020-01-20 21:37:58 -0500

    statring util

---
+++ JavaCurrent/JavaUtil/tryAndCatch/src/com/zeus/jim/Main.java
@@ -0,0 +1,41 @@
+package com.zeus.jim;
+
+import java.awt.*;
+import java.util.InputMismatchException;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(divideEAFP(2,0));
+
+//        arithmeticException error
+//        System.out.println(divide(2,0));
+
+
+        System.out.println(divideLBYL(2,0));
+
+    }
+
+    public static int divide(int num1, int num2){
+        return num1 / num2;
+    }
+
+//    Look Before You Leave
+    public static int divideLBYL(int num1, int num2 ){
+        if(num2 <= 0){
+            return 0;
+        }else{
+            return num1/ num2;
+        }
+    }
+
+//    Easy to Ask for Forgiveness and Permission
+    public static int divideEAFP(int num1, int num2){
+        try {
+            return num1 / num2;
+        }catch (ArithmeticException e){
+            return 0;
+        }
+    }
+}
