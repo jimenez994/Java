ee41fa4244e7723f2d10e2e76849d7a83d9b216f
JJimenez <j.jimenez.994@hotmail.com>
2020-01-26 11:18:43 -0500

    file reader and writter

---
+++ JavaCurrent/JavaUtil/InputOutput/src/com/zeus/jim/Locations.java
@@ -0,0 +1,135 @@
+package com.zeus.jim;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Locations implements Map<Integer, Location> {
+    private static Map<Integer, Location> locations = new HashMap<Integer, Location>();
+
+    public static void main(String[] args) throws IOException {
+        try(FileWriter locFile = new FileWriter("locations.txt");
+            FileWriter dirFile = new FileWriter("directions.txt")){
+            for (Location location: locations.values()) {
+                locFile.write(location.getLocationID() + ", " + location.getDescription() + "\n");
+                for (String direction: location.getExits().keySet()) {
+                    dirFile.write(location.getLocationID() + "," + direction + "," + location.getExits().get(direction) + "\n");
+                }
+            }
+        }
+//        FileWriter locFile  = null;
+//        try{
+//            locFile = new FileWriter("locations.txt");
+//            for (Location location: locations.values()) {
+//                locFile.write(location.getLocationID() + ", " + location.getDescription() + "\n");
+//            }
+//        }catch (IOException e) {
+//            System.out.println("In catch block");
+//            e.printStackTrace();
+//        }finally {
+//            System.out.println("in finally block");
+//            try{
+//                if(locFile != null){
+//                    System.out.println("Attempting to close locFile");
+//                    locFile.close();
+//                }
+//            }catch (IOException e){
+//                e.printStackTrace();
+//            }
+//        }
+    }
+
+    static {
+
+//        Map<String, Integer> tempExists = new HashMap<>();
+//        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java", tempExists));
+//
+//        tempExists = new HashMap<>();
+//        tempExists.put("N", 5);
+//        tempExists.put("E", 3);
+//        tempExists.put("S", 4);
+//        tempExists.put("W", 2);
+//        locations.put(1, new Location(1, "You are on the road", tempExists));
+//
+//        tempExists = new HashMap<>();
+//        tempExists.put("N", 5);
+//        locations.put(2, new Location(2, "You are at top o fa hill", tempExists));
+//
+//        tempExists = new HashMap<>();
+//        tempExists.put("W", 1);
+//        locations.put(3, new Location(3, "You are inside a building", tempExists));
+//
+//        tempExists = new HashMap<>();
+//        tempExists.put("N", 1);
+//        tempExists.put("W", 2);
+//        locations.put(4, new Location(4, "You are in a valley", tempExists));
+//
+//        tempExists = new HashMap<>();
+//        tempExists.put("W", 2);
+//        tempExists.put("S", 1);
+//        locations.put(5, new Location(5, "You are in th a forest", tempExists));
+    }
+
+    @Override
+    public int size() {
+        return locations.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return locations.isEmpty();
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return locations.containsKey(key);
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return locations.containsValue(value);
+    }
+
+    @Override
+    public Location get(Object key) {
+        return locations.get(key);
+    }
+
+    @Override
+    public Location put(Integer key, Location value) {
+        return locations.put(key, value);
+    }
+
+    @Override
+    public Location remove(Object key) {
+        return locations.remove(key);
+    }
+
+    @Override
+    public void putAll(Map<? extends Integer, ? extends Location> m) {
+
+    }
+
+    @Override
+    public void clear() {
+        locations.clear();
+    }
+
+    @Override
+    public Set<Integer> keySet() {
+        return locations.keySet();
+    }
+
+    @Override
+    public Collection<Location> values() {
+        return locations.values();
+    }
+
+    @Override
+    public Set<Entry<Integer, Location>> entrySet() {
+        return locations.entrySet();
+    }
+}
--- JavaCurrent/JavaUtil/InputOutput/src/com/zeus/jim/Main.java
+++ JavaCurrent/JavaUtil/InputOutput/src/com/zeus/jim/Main.java
@@ -1,8 +1,55 @@
 package com.zeus.jim;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
 
+    private static Locations locations = new Locations();
+    private static Map<String, String> compass = new HashMap<>();
+
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner scanner = new Scanner(System.in);
+
+        compass.put("NORTH", "N");
+        compass.put("EAST", "E");
+        compass.put("SOUTH", "S");
+        compass.put("WEST", "W");
+        compass.put("QUIT", "Q");
+
+        int loc = 1;
+        while (true){
+            System.out.println(locations.get(loc).getDescription());
+            if(loc == 0){
+                break;
+            }
+
+            Map<String, Integer> exits = locations.get(loc).getExits();
+            System.out.print("Available exists: ");
+
+            exits.forEach((direction, location) ->
+                System.out.print(direction +", ")
+            );
+
+            System.out.println();
+
+            String direction= scanner.nextLine().toUpperCase();
+            if(direction.length() > 1){
+                String[] userWords = direction.split(" ");
+                for (String word: userWords) {
+                    if(exits.containsKey(compass.get(word))){
+                        direction = compass.get(word);
+                        break;
+                    }
+                }
+            }
+            if(exits.containsKey(direction)){
+                loc = exits.get(direction);
+            }else{
+                System.out.println("Sorry you can't go that direction");
+            }
+        }
     }
 }
