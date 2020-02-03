e2186154c65229654c8f33dc6e6556ad80cbae29
JJimenez <j.jimenez.994@hotmail.com>
2020-02-02 20:26:56 -0500

    -

--- JavaCurrent/JavaUtil/InputOutput/src/com/zeus/jim/Locations.java
+++ JavaCurrent/JavaUtil/InputOutput/src/com/zeus/jim/Locations.java
@@ -2,6 +2,7 @@ package com.zeus.jim;
 
 import jdk.jshell.execution.LoaderDelegate;
 
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -59,6 +60,34 @@ public class Locations implements Map<Integer, Location> {
         }catch (IOException e){
             e.printStackTrace();
         }finally {
+            if(scanner != null){
+                scanner.close();
+            }
+        }
+        try{
+            scanner = new Scanner(new BufferedReader(new FileReader("directions.txt")));
+            scanner.useDelimiter(",");
+            while (scanner.hasNextLine()){
+//                this is a different way to do it
+//                int loc = scanner.nextInt();
+//                scanner.skip(scanner.delimiter());
+//                String direction = scanner.next();
+//                scanner.skip(scanner.delimiter());
+//                String dest = scanner.nextLine();
+//                int  destination = Integer.parseInt(dest);
+                String input = scanner.nextLine();
+                String[] data = input.split(",");
+                int loc = Integer.parseInt(data[0]);
+                String direction = data[1];
+                int destination = Integer.parseInt(data[2]);
+
+                System.out.println(loc + " : " + direction + " : " + destination);
+                Location location  = locations.get(loc);
+                location.addExit(direction, destination);
+            }
+        }catch (IOException e ){
+            e.printStackTrace();
+        }finally{
             if(scanner != null){
                 scanner.close();
             }
